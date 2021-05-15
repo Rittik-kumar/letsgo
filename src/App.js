17134df43ce3d1a207607f33c71ed8e9caf588ee
Rittik Kumar <rkrittik17@gmail.com>
2021-05-15 13:25:10 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1>let's go</h1>
-    </div>
+    <>
+      <Router>
+       <Navbar />
+       <Switch>
+         <Route path='/' exact />
+       </Switch>
+      </Router>
+    </>
   );
 }
 
