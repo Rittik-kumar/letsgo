dfbd5eb66b4a80fb977890645eb4bad694c8cd9a
Rittik Kumar <rkrittik17@gmail.com>
2021-05-21 09:52:34 +0530

    Setting up hero section

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Router>
        <Navbar />
        <Switch>
-         <Route path='/' exact  components={Home} />
+         <Route path='/' exact  component={Home} />
        </Switch>
       </Router>
     </>
